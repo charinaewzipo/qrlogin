3ecb789e8eae9f9910ef3a5d430930a80e6d01be
tanupat <tanupat.boonruksa@gmail.com>
2023-03-07 15:56:56 +0700

    เพิ่ม GuestGuard // ลบ GuestGuard

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -3,6 +3,7 @@ import { Stack, Typography } from '@mui/material'
 import Head from 'next/head'
 import ForgotPasswordForm from '@ku/components/ForgotPassword/ForgotPasswordForm'
 import LoginLayout from '@ku/layouts/login'
+import GuestGuard from '@ku/guard/GuestGuard'
 
 // ----------------------------------------------------------------------
 
@@ -15,14 +16,16 @@ export default function ForgotPasswordPage() {
                 <title> Forgot password | KU </title>
             </Head>
 
-            <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
-                <Typography variant="h4" align="center">Forgot your password?</Typography>
-                <Stack direction="column" spacing={0.5}>
-                    <Typography variant="body2" align="center">Please enter the email address associated with your account,</Typography>
-                    <Typography variant="body2" align="center">and we'll email you a link to reset your password.</Typography>
+            <GuestGuard>
+                <Stack spacing={2} sx={{ mb: 5, position: 'relative' }}>
+                    <Typography variant="h4" align="center">Forgot your password?</Typography>
+                    <Stack direction="column" spacing={0.5}>
+                        <Typography variant="body2" align="center">Please enter the email address associated with your account,</Typography>
+                        <Typography variant="body2" align="center">and we'll email you a link to reset your password.</Typography>
+                    </Stack>
                 </Stack>
-            </Stack>
-            <ForgotPasswordForm />
+                <ForgotPasswordForm />
+            </GuestGuard>
         </>
     )
 }
