2b0a0dc23dac793839c04322ae599425518106a2
Pakorn233 <pakorn233@gmail.com>
2023-03-08 17:57:50 +0700

    แก้ margin ให้ตรง figma

--- pages/register/success.tsx
+++ pages/register/success.tsx
@@ -32,90 +32,89 @@ const RegisterSuccess = () => {
 
             <GuestGuard>
                 <ContentStyle>
-                    <Stack>
-                        <Stack spacing={2} sx={{ position: 'relative', mt: 12 }}>
-                            <Typography variant="h5" textAlign="center">
-                                Thank you for your registered!
-                            </Typography>
-                            <Box
-                                component="div"
-                                sx={{
-                                    display: 'flex',
-                                    justifyContent: 'center',
-                                }}
-                            >
-                                <Image
-                                    alt="Illustration register"
-                                    src={'/assets/illustrations/illustration_register.png'}
-                                    sx={{ objectFit: 'cover', my: 8, width: 327.33 }}
-                                    disabledEffect
-                                />
-                            </Box>
-                            <Stack direction="column" spacing={3}>
-                                <Typography variant="body1">
-                                    {`Thanks for your registered.`}
-                                    <Typography
-                                        variant="body1"
-                                        sx={{ color: theme.palette.primary.main }}
-                                    >
-                                        {isEmpty(router.query) ? '' : get(router, 'query.name', '')}
-                                    </Typography>
-                                </Typography>
-                                {get(router, 'query.isStudent', false) === 'true' ? (
-                                    <Typography variant="body1" sx={{ my: 2 }}>
-                                        {`The request has been sent to your supervisor\n`}"
-                                        <Typography
-                                            variant="body1"
-                                            component="span"
-                                            sx={{ color: theme.palette.text.secondary }}
-                                        >
-                                            supervisor.email@mail.com
-                                        </Typography>
-                                        "{` please waiting for\n`}
-                                        {`approval`}
-                                    </Typography>
-                                ) : (
-                                    <></>
-                                )}
-
-                                <Typography variant="body1">All the best,</Typography>
-                            </Stack>
-                        </Stack>
-
+                    <Stack spacing={2} sx={{ position: 'relative', mt: 12 }}>
+                        <Typography variant="h5" textAlign="center">
+                            Thank you for your registered!
+                        </Typography>
                         <Box
+                            component="div"
                             sx={{
                                 display: 'flex',
-                                mt: 4,
-                                borderBottom: `1px solid ${theme.palette.divider}`,
-                            }}
-                        ></Box>
-
-                        <Button
-                            variant="outlined"
-                            sx={{
-                                border: `1px solid ${theme.palette.divider}`,
-                                color: theme.palette.text.primary,
-                                display: 'flex',
-                                alignItems: 'center',
                                 justifyContent: 'center',
-                                mt: 3,
                             }}
                         >
                             <Image
-                                alt="Icon Base"
-                                src={'/assets/icons/direction/Icon Base.png'}
-                                sx={{ objectFit: 'cover', height: 20.67, mr: 1 }}
+                                alt="Illustration register"
+                                src={'/assets/illustrations/illustration_register.png'}
+                                sx={{ objectFit: 'cover', my: 6, width: 327.33 }}
+                                disabledEffect
                             />
-                            <Link
-                                href={{
-                                    pathname: LOGIN_PATH,
-                                }}
-                                style={{ textDecoration: 'none', color: 'inherit' }}
-                            >
-                                {' '}Back to Login page
-                            </Link>
-                        </Button>
+                        </Box>
+                        <Stack direction="column" spacing={3}>
+                            <Typography variant="body1">
+                                {`Thanks for your registered.`}
+                                <Typography
+                                    variant="body1"
+                                    sx={{ color: theme.palette.primary.main }}
+                                >
+                                    {isEmpty(router.query) ? '' : get(router, 'query.name', '')}
+                                </Typography>
+                            </Typography>
+                            {get(router, 'query.isStudent', false) === 'true' ? (
+                                <Typography variant="body1" sx={{ my: 2 }}>
+                                    {`The request has been sent to your supervisor\n`}"
+                                    <Typography
+                                        variant="body1"
+                                        component="span"
+                                        sx={{ color: theme.palette.text.secondary }}
+                                    >
+                                        supervisor.email@mail.com
+                                    </Typography>
+                                    "{` please waiting for\n`}
+                                    {`approval`}
+                                </Typography>
+                            ) : (
+                                <></>
+                            )}
+
+                            <Typography variant="body1">All the best,</Typography>
+                        </Stack>
                     </Stack>
+
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            mt: 4,
+                            borderBottom: `1px solid ${theme.palette.divider}`,
+                        }}
+                    ></Box>
+
+                    <Button
+                        variant="outlined"
+                        sx={{
+                            border: `1px solid ${theme.palette.divider}`,
+                            color: theme.palette.text.primary,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            mt: 3,
+                        }}
+                    >
+                        <Image
+                            alt="Icon Base"
+                            src={'/assets/icons/direction/Icon Base.png'}
+                            sx={{ objectFit: 'cover', height: 20.67, mr: 1 }}
+                        />
+                        <Link
+                            href={{
+                                pathname: LOGIN_PATH,
+                            }}
+                            style={{ textDecoration: 'none', color: 'inherit' }}
+                        >
+                            {' '}
+                            Back to Login page
+                        </Link>
+                    </Button>
                 </ContentStyle>
             </GuestGuard>
         </>
