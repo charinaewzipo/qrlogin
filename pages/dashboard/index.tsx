6e1270510fe4ad6c15b3ffac4510cb7d3168f498
Namasawee Meekrongbang <dev.applelemon@gmail.com>
2023-03-08 13:24:43 +0700

    เปลี่ยน span -> tyrography

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,18 +2,18 @@ import { m, useScroll, useSpring } from 'framer-motion'
 import styles from '../../styles/index.module.scss'
 import Head from 'next/head'
 import { useTheme } from '@mui/material/styles'
-import { Box, Grid, Container } from '@mui/material'
+import { Box, Grid, Container, Typography } from '@mui/material'
 import AuthorizedLayout from '@ku/layouts/authorized'
 import CustomBreadcrumbs from '@sentry/components/custom-breadcrumbs'
 import { AnalyticsWidgetSummary } from '../../components/Dashboard/analytics'
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next'
 
 Index.getLayout = (page: React.ReactElement) => <AuthorizedLayout> {page} </AuthorizedLayout>
 declare type PERMISSION = 'Admin' | 'Finance' | 'Supervisor' | 'User'
 
 export function Index() {
     const theme = useTheme()
-    const { t } = useTranslation();
+    const { t } = useTranslation()
 
     const { scrollYProgress } = useScroll()
 
@@ -23,8 +23,7 @@ export function Index() {
         restDelta: 0.001,
     })
 
-
-    const permission : PERMISSION = 'Admin'
+    const permission: PERMISSION = 'Supervisor'
 
     const progress = (
         <m.div
@@ -64,7 +63,7 @@ export function Index() {
                                 sx={{ mt: 3, mb: 0, height: 72 }}
                             />
 
-                            {permission === 'Admin' ? (
+                            {(permission as PERMISSION) === 'Admin' ? (
                                 <Grid container spacing={2} sx={{ mt: 3 }}>
                                     <Grid item xs={12} sm={6} md={3}>
                                         <AnalyticsWidgetSummary
@@ -72,8 +71,7 @@ export function Index() {
                                             total={2}
                                             color="grey"
                                             icon={'mdi:bag-personal'}
-                                            units='Booking'
-                                            
+                                            units="Booking"
                                         />
                                     </Grid>
 
@@ -83,8 +81,7 @@ export function Index() {
                                             total={581}
                                             color="warning"
                                             icon={'icon-park-outline:done-all'}
-                                            units='Booking'
-                                           
+                                            units="Booking"
                                         />
                                     </Grid>
                                     <Grid item xs={12} sm={6} md={3}>
@@ -92,7 +89,7 @@ export function Index() {
                                             title="Confirm"
                                             total={18}
                                             icon={'ic:baseline-verified'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
 
@@ -102,20 +99,19 @@ export function Index() {
                                             total={12}
                                             color="info"
                                             icon={'ant-design:credit-card-filled'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
                                 </Grid>
-                            ) : permission === 'Finance' ? (
+                            ) : (permission as PERMISSION) === 'Finance' ? (
                                 <Grid container spacing={2} sx={{ mt: 3 }}>
-                    
                                     <Grid item xs={12} sm={6} md={3}>
                                         <AnalyticsWidgetSummary
                                             title="Waiting for payment"
                                             total={12}
                                             color="grey"
                                             icon={'ant-design:credit-card-filled'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
 
@@ -123,89 +119,113 @@ export function Index() {
                                         <AnalyticsWidgetSummary
                                             title="All booking"
                                             total={128}
-                                            color='info'
+                                            color="info"
                                             icon={'mdi:bag-personal'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
                                 </Grid>
-                            ) : permission === 'Supervisor' ? (
+                            ) : (permission as PERMISSION) === 'Supervisor' ? (
                                 <>
-                                <div style={{marginTop:40}}>
-                                {/* {t("en.only_admin_can_see_this_item")} */}
-                                <p style={{ fontSize:16,fontWeight:600,color:'#637381'}}>Supervisor Code</p>
-                                <p style={{ fontSize:32,fontWeight:700,color:'#212B36'}}>AA5643GN</p>
-                                </div>
-                                <Grid container spacing={2} sx={{ mt: 3 }}>
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="All Booking"
-                                            total={5}
-                                            color={'grey'}
-                                            icon={'mdi:bag-personal'}
-                                            units='Booking'
-                                        />
-                                    </Grid>
-
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="Waiting for payment"
-                                            total={12}
-                                            color="info"
-                                            icon={'ant-design:credit-card-filled'}
-                                            units='Booking'
-                                        />
-                                    </Grid>
-
-                                  
-                                </Grid>
-
-                                <div style={{marginTop:40}}>
-                                    <span style={{ fontSize:16,fontWeight:600,color:'#212B36'}}>Students/Staff</span> 
-                                    <span style={{fontSize:16,fontWeight:600,color:'#919EAB'}}> (Under control)</span>
+                                    <div style={{ marginTop: 40 }}>
+                                        {/* {t("en.only_admin_can_see_this_item")} */}
+                                        <p
+                                            style={{
+                                                fontSize: 16,
+                                                fontWeight: 600,
+                                                color: theme.palette.grey[600],
+                                            }}
+                                        >
+                                            Supervisor Code
+                                        </p>
+                                        <p
+                                            style={{
+                                                fontSize: 32,
+                                                fontWeight: 700,
+                                                color: theme.palette.grey[800],
+                                            }}
+                                        >
+                                            AA5643GN
+                                        </p>
                                     </div>
-                                <Grid container spacing={2} sx={{ mt: 3 }}>
-                                 
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="Student"
-                                            total={450}
-                                            color={'grey'}
-                                            icon={'fa6-solid:user-group'}
-                                            units='Users'
-                                        />
+                                    <Grid container spacing={2} sx={{ mt: 3 }}>
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="All Booking"
+                                                total={5}
+                                                color={'grey'}
+                                                icon={'mdi:bag-personal'}
+                                                units="Booking"
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="Waiting for payment"
+                                                total={12}
+                                                color="info"
+                                                icon={'ant-design:credit-card-filled'}
+                                                units="Booking"
+                                            />
+                                        </Grid>
                                     </Grid>
 
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="All Booking"
-                                            total={320}
-                                            color="grey"
-                                            icon={'mdi:bag-personal'}
-                                            units='Booking'
-                                        />
-                                    </Grid>
+                                    <div style={{ marginTop: 40 }}>
+                                        <Typography variant="subtitle1" noWrap color="text.primary">
+                                            Students/Staff
+                                            <Typography
+                                                variant="subtitle1"
+                                                color={theme.palette.grey[500]}
+                                                noWrap
+                                                component="span"
+                                            >
+                                                {' '}
+                                                (Under control)
+                                            </Typography>
+                                        </Typography>
+                                    </div>
 
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="Pending"
-                                            total={15}
-                                            color="warning"
-                                            icon={'icon-park-outline:done-all'}
-                                            units='Booking'
-                                        />
-                                    </Grid>
+                                    <Grid container spacing={2} sx={{ mt: 3 }}>
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="Student"
+                                                total={450}
+                                                color={'grey'}
+                                                icon={'fa6-solid:user-group'}
+                                                units="Users"
+                                            />
+                                        </Grid>
 
-                                    <Grid item xs={12} sm={6} md={3}>
-                                        <AnalyticsWidgetSummary
-                                            title="Waiting for payment"
-                                            total={12}
-                                            color="info"
-                                            icon={'ant-design:credit-card-filled'}
-                                            units='Booking'
-                                        />
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="All Booking"
+                                                total={320}
+                                                color="grey"
+                                                icon={'mdi:bag-personal'}
+                                                units="Booking"
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="Pending"
+                                                total={15}
+                                                color="warning"
+                                                icon={'icon-park-outline:done-all'}
+                                                units="Booking"
+                                            />
+                                        </Grid>
+
+                                        <Grid item xs={12} sm={6} md={3}>
+                                            <AnalyticsWidgetSummary
+                                                title="Waiting for payment"
+                                                total={12}
+                                                color="info"
+                                                icon={'ant-design:credit-card-filled'}
+                                                units="Booking"
+                                            />
+                                        </Grid>
                                     </Grid>
-                                </Grid>
                                 </>
                             ) : (
                                 <Grid container spacing={2} sx={{ mt: 3 }}>
@@ -215,7 +235,7 @@ export function Index() {
                                             total={5}
                                             color="grey"
                                             icon={'mdi:bag-personal'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
 
@@ -225,7 +245,7 @@ export function Index() {
                                             total={1}
                                             color="warning"
                                             icon={'icon-park-outline:done-all'}
-                                            units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
 
@@ -235,7 +255,7 @@ export function Index() {
                                             total={0}
                                             color={'info'}
                                             icon={'ant-design:credit-card-filled'}
-                                             units='Booking'
+                                            units="Booking"
                                         />
                                     </Grid>
                                 </Grid>
